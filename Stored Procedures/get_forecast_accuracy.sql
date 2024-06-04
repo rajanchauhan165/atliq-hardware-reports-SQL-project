07e41c58c5f16bcececf197c04ece979f7ea4ddd
Rajan Chauhan <rajanchauhan165@gmail.com>
2024-06-04 23:51:54 +0530

    supply chain script

---
+++ Stored Procedures/get_forecast_accuracy.sql
@@ -0,0 +1,19 @@
+CREATE PROCEDURE `get_forecast_accuracy` (IN in_fy INT)
+BEGIN
+
+WITH forecast_error_table AS
+(SELECT *,
+(forecast_quantity-sold_quantity) AS net_error,
+100*SUM((forecast_quantity-sold_quantity))/SUM(forecast_quantity) AS net_error_pct,
+SUM(ABS(forecast_quantity-sold_quantity)) AS abs_error,
+100*SUM((ABS(forecast_quantity-sold_quantity)))/SUM(forecast_quantity) AS abs_error_pct
+FROM fact_act_est
+WHERE fiscal_year=in_fy
+GROUP BY customer_code)
+
+SELECT e.*, c.customer, c.market,
+IF(abs_error_pct > 100, 0, 100-abs_error_pct) AS forecast_accuracy
+FROM forecast_error_table e JOIN dim_customer c ON e.customer_code=c.customer_code
+ORDER BY forecast_accuracy;
+
+END
